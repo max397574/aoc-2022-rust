4205c5078ec679ee52b67b55e6123c97cfad61ba
Max <max.heinzer397@gmx.ch>
2022-12-07 22:13:34 +0100

    refactor(day1, part2): rewrite without loops and with much less variables

--- src/year_2022/day_1.rs
+++ src/year_2022/day_1.rs
@@ -12,28 +12,32 @@ fn part_1(input: &str) -> usize {
 }
 
 #[aoc(day1, part2)]
-fn part2(input: &str) -> i32 {
-    let lines = input.split('\n');
-    let mut max1 = 0;
-    let mut max2 = 0;
-    let mut max3 = 0;
-    let mut current: i32 = 0;
-    for line in lines {
-        if !line.is_empty() {
-            current += line.parse::<i32>().expect("expected number");
-        } else {
-            if current > max1 {
-                max3 = max2;
-                max2 = max1;
-                max1 = current;
-            } else if current > max2 {
-                max3 = max2;
-                max2 = current
-            } else if current > max3 {
-                max3 = current;
-            }
-            current = 0
-        }
+fn part_2(input: &str) -> usize {
+    let mut sorted = input
+        .split("\n\n")
+        .map(|set| {
+            set.lines()
+                .map(|line| line.parse::<usize>().unwrap())
+                .sum::<usize>()
+        })
+        .collect::<Vec<usize>>();
+    sorted.sort();
+    let length = sorted.len();
+    sorted.into_iter().skip(length- 3).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT: &str = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
+
+    #[test]
+    fn part1() {
+        assert_eq!(part_1(INPUT), 24000);
     }
-    max1 + max2 + max3
+
+    #[test]
+    fn part2() {
+        assert_eq!(part_2(INPUT), 45000);
+    }
 }
