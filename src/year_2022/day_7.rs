ae8692366cb72eeac5f0b7ee458622b5b3ac9a71
Max <max.heinzer397@gmx.ch>
2022-12-07 14:02:16 +0100

    feat: add day7

--- src/year_2022/day_7.rs
+++ src/year_2022/day_7.rs
@@ -1,6 +1,57 @@
-#[aoc_generator(day7)]
-fn generator(input: &str) -> _ {}
+use std::collections::HashMap;
+
 #[aoc(day7, part1)]
-fn entry(input: &_) -> usize {
-    0
+fn entry(input: &str) -> usize {
+    let lines = input.split('\n');
+    let mut cwd: Vec<String> = Vec::new();
+    let mut dir_sizes = HashMap::new();
+    for line in lines {
+        match line.split(' ').collect::<Vec<&str>>()[..] {
+            ["$", "cd", "/"] => {
+                cwd = vec!["".to_string()];
+            }
+            ["$", "cd", ".."] => {
+                cwd.pop();
+            }
+            ["$", "cd", x] => {
+                cwd.push(x.to_string());
+            }
+            ["$", "ls"] => {}
+            ["dir", _] => {}
+            [size, _] => {
+                let mut dir_name = String::from("");
+                for dir in cwd.iter() {
+                    if !dir.is_empty() && dir_name != "/" {
+                        dir_name.push('/');
+                    }
+                    dir_name.push_str(dir);
+                    if cwd == [""] {
+                        dir_name = String::from("/")
+                    }
+                    if dir_name.is_empty() {
+                        dir_name = String::from("/")
+                    }
+                    if !dir_sizes.contains_key(&dir_name) {
+                        dir_sizes.insert(dir_name.clone(), size.parse::<usize>().unwrap());
+                    } else {
+                        dir_sizes.insert(
+                            dir_name.clone(),
+                            dir_sizes.get(&dir_name).unwrap() + size.parse::<usize>().unwrap(),
+                        );
+                    }
+                }
+            }
+            _ => {
+                unreachable!();
+            }
+        }
+    }
+    let mut sum = 0;
+    for (dir, size) in dir_sizes {
+        println!("{dir}:{size}");
+        if size <= 100000 {
+            sum += size
+        }
+    }
+    sum
 }
