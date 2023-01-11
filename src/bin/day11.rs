50db2df8c5f1935f3b915fa799dfae13181d0dd1
Max <max.heinzer397@gmx.ch>
2023-01-11 21:29:51 +0100

    feat: extract gcd and lcm functions to `lib.rs`

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,3 +1,4 @@
+use aoc::get_lcm;
 use std::collections::VecDeque;
 
 #[derive(Clone, Copy, Debug)]
@@ -113,30 +114,6 @@ fn part_1(monkeys: &mut [Monkey]) -> usize {
         .collect::<Vec<_>>();
     inspections.sort_unstable();
     inspections[monkeys.len() - 2..].iter().product()
-}
-
-fn gcd(a: usize, b: usize) -> usize {
-    let mut a = a;
-    let mut b = b;
-    let mut c;
-    while b != 0 {
-        c = a;
-        a = b;
-        b = c % b;
-    }
-    a
-}
-
-fn single_lcm(a: usize, b: usize) -> usize {
-    (a * b) / gcd(a, b)
-}
-
-fn get_lcm(numbers: Vec<usize>) -> usize {
-    let mut tmp_lcm = single_lcm(*numbers.first().unwrap(), *numbers.get(1).unwrap());
-    for num in numbers.iter().skip(2) {
-        tmp_lcm = single_lcm(tmp_lcm, *num);
-    }
-    tmp_lcm
 }
 
 fn part_2(monkeys: &mut [Monkey]) -> usize {
---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+pub fn gcd(a: usize, b: usize) -> usize {
+    let mut a = a;
+    let mut b = b;
+    let mut c;
+    while b != 0 {
+        c = a;
+        a = b;
+        b = c % b;
+    }
+    a
+}
+
+pub fn single_lcm(a: usize, b: usize) -> usize {
+    (a * b) / gcd(a, b)
+}
+
+pub fn get_lcm(numbers: Vec<usize>) -> usize {
+    let mut tmp_lcm = single_lcm(*numbers.first().unwrap(), *numbers.get(1).unwrap());
+    for num in numbers.iter().skip(2) {
+        tmp_lcm = single_lcm(tmp_lcm, *num);
+    }
+    tmp_lcm
+}
